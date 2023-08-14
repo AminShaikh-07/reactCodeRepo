4fd8ebb334488dae4ff7f8ebc0615129a7f622c8
AminShaikh-07 <109506663+AminShaikh-07@users.noreply.github.com>
2023-08-14 15:07:14 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+//Hooks in React starts with 'use' keyword
+
+import React from 'react';
+import './style.css';
+import { useState, useEffect } from 'react';
+import Child from './Child.jsx';
 
 export default function App() {
+  const [name, setName] = useState('kc'); // Hooks are function which is called here and it returns array with two element
+  // first element is the actual value
+  // second element is setName kinda function which changes a value of the state
+
+  const age = 20;
+  const address = "Mumbai"
+  useEffect(() => {
+    console.log('effect called');
+  }, [name]);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello {name}</h1>
+      <button onClick={() => setName('VJTI')}>Change College</button>
+      <button onClick={()=> setName('KC')}>Reset</button>
+      <Child age={age} address={address}/>
     </div>
   );
 }
---
+++ src/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Child = (props) => {
+  return (
+    <>
+      <h2>Hello I am a Full Stack Web Developer . I am {props.age} yrs old . I stay in {props.address}</h2>
+    </>
+  );
+};
+
+export default Child;
